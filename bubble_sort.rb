0e0233b8b0b69def1d7db5179c55c163ab615619
omokoro <omokoro@gmail.com>
2015-03-21 13:00:34 -0500

    completed project

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,14 +4,30 @@ def bubble_sort(numbers)
 		sorted = true
 		for i in 1..numbers.length-1
 			if numbers[i-1] > numbers[i]
-				temp = numbers[i]
 				numbers[i-1], numbers[i] = numbers[i], numbers[i-1]
 				sorted = false
 			end
-			
 		end
 	end
-	p numbers
+	numbers
 end
 
-bubble_sort([4,3,78,2,0,2])
+def bubble_sort_by(array)
+	sorted = false 
+	until sorted
+		sorted = true
+		for i in 1..array.length-1
+			if yield(array[i-1], array[i]) < 0
+				array[i-1], array[i] = array[i], array[i-1]
+				sorted = false
+			end
+		end
+	end
+	array
+end
+
+
+p bubble_sort([4,3,78,2,0,2])
+
+p bubble_sort_by(["hi","hello","hey"]) { |l, r| r.length - l.length }
+
